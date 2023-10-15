fa5d95c65d01496cf78330f0c18a2503380851f5
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2023-10-16 00:57:29 +0900

    Update list panel

--- rtui/widgets/__init__.py
+++ rtui/widgets/__init__.py
@@ -1,10 +1,12 @@
 from .info_panel import RosEntityInfoPanel
+from .list_panel import RosEntityListPanel
 
 # from .separator import Separator
 # from .topic import TopicEcho
 
 __all__: list[str] = [
     "RosEntityInfoPanel",
+    "RosEntityListPanel",
     # "Separator",
     # "TopicEcho",
 ]
