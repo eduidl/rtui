4a0f4a177d15c5d2a11bfdbb750069c5b2850a55
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2023-10-22 20:11:29 +0900

    Remove Separator

--- rtui/widgets/__init__.py
+++ rtui/widgets/__init__.py
@@ -1,10 +1,7 @@
 from .info_panel import RosEntityInfoPanel
 from .list_panel import RosEntityListPanel
 
-# from .separator import Separator
-
 __all__ = [
     "RosEntityInfoPanel",
     "RosEntityListPanel",
-    # "Separator",
 ]
