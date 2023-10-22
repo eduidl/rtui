9fc1f13a5c190aa532cab43cc5e36e2ec332c465
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2023-10-22 14:39:54 +0900

    Remove TopicEcho widget

--- rtui/widgets/__init__.py
+++ rtui/widgets/__init__.py
@@ -1,10 +1,7 @@
 from .info_panel import RosEntityInfoPanel
 from .list_panel import RosEntityListPanel
 
-# from .topic import TopicEcho
-
 __all__: list[str] = [
     "RosEntityInfoPanel",
     "RosEntityListPanel",
-    # "TopicEcho",
 ]
